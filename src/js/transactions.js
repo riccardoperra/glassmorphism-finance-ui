b108dea4ee02031c3ab906717fd8fcc00a8e6897
Riccardo Perra <riccardo.perra@icloud.com>
2021-08-10 16:39:48 +0200

    fix transaction list code generation

---
+++ src/js/transactions.js
@@ -0,0 +1,30 @@
+export const transactions = [
+  {
+    initial: 'JD',
+    name: 'John Doe',
+    type: 'Money received',
+    date: '10/08/2021',
+    price: '20,00 EUR',
+  },
+  {
+    initial: 'G',
+    name: 'Google',
+    type: 'Automatic payment',
+    date: '09/08/2021',
+    price: '1,99 EUR',
+  },
+  {
+    initial: 'JD',
+    name: 'John Doe',
+    type: 'Refunded',
+    date: '04/04/2021',
+    price: '150,00 EUR',
+  },
+  {
+    initial: 'JD',
+    name: 'John Doe',
+    type: 'Money sent',
+    date: '10/02/2020',
+    price: '10,00 EUR',
+  },
+];
